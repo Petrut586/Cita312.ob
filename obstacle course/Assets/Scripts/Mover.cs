47bfac0df1af8cb2c6aa3135e4bfd7eaaf9a6676
petrut586 <petrut586@morrisville.edu>
2022-09-07 11:31:33 -0400

    Unity Obstacle Course

---
+++ obstacle course/Assets/Scripts/Mover.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PrintInstruction();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    void PrintInstruction()//prints a welcome and instructions to console
+    {
+        Debug.Log("Welcome to the game");
+        Debug.Log("Move your player with WASD or arrow keys");
+        Debug.Log("Don't hit the walls!");
+    }
+
+    void MovePlayer()//allows the user to move the player
+    {
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+
+        transform.Translate(xValue, 0, zValue);
+    }
+}
