3c91d5eb36554949a7592761ef342d5a8534e232
petrut586 <petrut586@morrisville.edu>
2022-09-14 16:55:22 -0400

    Boost Day 2

--- 312_Boost/Boost/Assets/Scripts/Movement.cs
+++ 312_Boost/Boost/Assets/Scripts/Movement.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    Rigidbody rb;
+    [SerializeField] float mainThrust = 0;
+    [SerializeField] float rotationThrust = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -21,19 +25,28 @@ public class Movement : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space))//Apply thrust to rocket
         {
-            
+            rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
         }       
     }
 
     void ProcessRotation()
     {
-        if (Input.GetKey(KeyCode.A))//Rotate rocket left
-        {
 
-        }
-        else if (Input.GetKey(KeyCode.D))//Rotate rocket right
-        {
 
+        if (Input.GetKey(KeyCode.A))//Rotate rocket right
+        {
+            ApplyRotation(rotationThrust);
         }
+        else if (Input.GetKey(KeyCode.D))//Rotate rocket left
+        {
+            ApplyRotation(-rotationThrust);
+        }
+    }
+
+    void ApplyRotation(float rotationThisFrame)
+    {
+        rb.freezeRotation = true; // freezing rotation so we can manually rotate
+        transform.Rotate(Vector3.forward * rotationThisFrame * Time.deltaTime);
+        rb.freezeRotation = false; // un-freezing rotation
     }
 }
