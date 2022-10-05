22878ff79d06f7a61b86f776e91e7688ea525a23
petrut586 <petrut586@morrisville.edu>
2022-10-05 12:24:37 -0400

    Completed Boost Project

--- 312_Boost/Boost/Assets/Scripts/CollisionHandler.cs
+++ 312_Boost/Boost/Assets/Scripts/CollisionHandler.cs
@@ -5,9 +5,42 @@ using UnityEngine.SceneManagement;
 public class CollisionHandler : MonoBehaviour
 {
     [SerializeField] float levelLoadDelay = 2;
+    [SerializeField] AudioClip crash;
+    [SerializeField] AudioClip success;
+    [SerializeField] ParticleSystem successParticles;
+    [SerializeField] ParticleSystem crashParticles;
+
+    AudioSource audioSource;
+
+    bool isTransitioning = false;
+    bool collisionDisabled = false;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        RespondToDebugKeys();
+    }
+
+    void RespondToDebugKeys()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            NextLevel();
+        }
+        else if (Input.GetKeyDown(KeyCode.C))
+        {
+            collisionDisabled = !collisionDisabled; // toggle collision
+        }
+    }
 
     void OnCollisionEnter(Collision collision)
     {
+        if(isTransitioning || collisionDisabled) { return; }
+
         switch (collision.gameObject.tag)
         {
             case "Friendly":
@@ -22,18 +55,23 @@ public class CollisionHandler : MonoBehaviour
         }
     }
 
-    private void StartSuccessSequence()
+    void StartSuccessSequence()
     {
-        //todo add SFX upon crash
-        //todo add particle effect upon crash
+        isTransitioning = true;
+        audioSource.Stop();
+        audioSource.PlayOneShot(success);
+        successParticles.Play();
         GetComponent<Movement>().enabled = false;
+        
         Invoke("NextLevel", levelLoadDelay);
     }
 
     void StartCrashSequence()
     {
-        //todo add SFX upon crash
-        //todo add particle effect upon crash
+        isTransitioning = true;
+        audioSource.Stop();
+        audioSource.PlayOneShot(crash);
+        crashParticles.Play();
         GetComponent<Movement>().enabled = false;
         Invoke("ReloadLevel", levelLoadDelay);
     }
@@ -55,4 +93,5 @@ public class CollisionHandler : MonoBehaviour
         }
         SceneManager.LoadScene(nextSceneIndex);
     }
+
 }
--- 312_Boost/Boost/Assets/Scripts/Movement.cs
+++ 312_Boost/Boost/Assets/Scripts/Movement.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    [SerializeField] float mainThrust = 100f;
+    [SerializeField] float rotationThrust = 1f;
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] ParticleSystem mainEngineParticles;
+    [SerializeField] ParticleSystem rightThrusterParticles;
+    [SerializeField] ParticleSystem leftThrusterParticles;
+
     Rigidbody rb;
     AudioSource audioSource;
-    [SerializeField] float mainThrust = 0;
-    [SerializeField] float rotationThrust = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -27,17 +32,32 @@ public class Movement : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space))//Apply thrust to rocket
         {
-            rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
-            if (!audioSource.isPlaying)
-            {
-                audioSource.Play();
-            }
-            
+            StartThrusting();
         }
         else
         {
-            audioSource.Stop();
+            StopThrusting();
         }
+    }
+
+
+    void StartThrusting()
+    {
+        rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+        if (!audioSource.isPlaying)
+        {
+            audioSource.PlayOneShot(mainEngine);
+        }
+        if (!mainEngineParticles.isPlaying)
+        {
+            mainEngineParticles.Play();
+        }
+    }
+
+    void StopThrusting()
+    {
+        audioSource.Stop();
+        mainEngineParticles.Stop();
     }
 
     void ProcessRotation()
@@ -46,11 +66,40 @@ public class Movement : MonoBehaviour
 
         if (Input.GetKey(KeyCode.A))//Rotate rocket right
         {
-            ApplyRotation(rotationThrust);
+            RotateRight();
+
         }
         else if (Input.GetKey(KeyCode.D))//Rotate rocket left
         {
-            ApplyRotation(-rotationThrust);
+            RotateLeft();
+        }
+        else
+        {
+            StopRotating();
+        }
+    }
+
+    void StopRotating()
+    {
+        rightThrusterParticles.Stop();
+        leftThrusterParticles.Stop();
+    }
+
+    void RotateLeft()
+    {
+        ApplyRotation(-rotationThrust);
+        if (!leftThrusterParticles)
+        {
+            leftThrusterParticles.Play();
+        }
+    }
+
+    void RotateRight()
+    {
+        ApplyRotation(rotationThrust);
+        if (!rightThrusterParticles)
+        {
+            rightThrusterParticles.Play();
         }
     }
 
